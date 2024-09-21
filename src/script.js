fe80e85b860bc71a5975ada72761ebc474a1b036
lea-bee <twigg.lt20@gmail.com>
2024-09-21 13:26:44 +0200

    Added basic js and Typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem-box", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemForm = document.querySelector("#poem-generator-form");
+poemForm.addEventListener("submit", generatePoem);
